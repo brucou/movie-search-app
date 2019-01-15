88a1281ca827af3f3aed684c964b5fff5d449a2c
brucou <bruno.couriol@gmail.com>
2019-01-15 20:24:08 +0100

    chore(TEsT)

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,9 @@
 import './test-S0'
 import './test-S1'
 import './test-S2'
+import './test-S4'
 QUnit.dump.maxDepth = 20;
-
+QUnit.onUnhandledRejection = ()=>{};
 
 // to get string version without loosing undefined through JSON conversion
 // JSON.stringify(hash, (k, v) => (v === undefined) ? '__undefined' : v)
